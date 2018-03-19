478362c717c30417c2264f8697cd201434aeba48
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-03-19 11:05:21 -0700

    rake db:migrate

--- db/migrate/20180319175554_create_families.rb
+++ db/migrate/20180319175554_create_families.rb
@@ -1,7 +1,8 @@
 class CreateFamilies < ActiveRecord::Migration[5.1]
   def change
     create_table :families do |t|
-
+      t.string :name
+      t.integer :owner_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319011605) do
+ActiveRecord::Schema.define(version: 20180319175554) do
+
+  create_table "families", force: :cascade do |t|
+    t.string "name"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
