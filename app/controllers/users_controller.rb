161432a04955686a50089a9cfb2969fbd701bf4e
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-03-18 18:48:56 -0700

    gracefully handle a RecordNotFound

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
   def create
     u = User.new
     u.email = params[:email]
     u.password = params[:password]
     if u.save!
       render json: u, status: 200
-    else
-      render status: 400
     end
   end
 
   def show
+    if User.find(params[:id])
+      render json: User.find(params[:id]), status: 200
+    end
   end
 
   def update
+    u = User.find(params[:id])
+  end
+
+  private
+
+  def render_404
+    render json: {status: 404, message: 'failed'}
   end
 end
